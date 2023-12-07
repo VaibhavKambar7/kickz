bb11d13c6596dd8f9039993eb7eded5f1d233d4d
VaibhavKambar7 <vaibhavkambar123@gmail.com>
2023-12-08 01:28:16 +0530

    added related product carousel in product detail page

---
+++ src/app/components/RelatedProducts.tsx
@@ -0,0 +1,96 @@
+"use client";
+
+import React from "react";
+import Carousel from "react-multi-carousel";
+import "react-multi-carousel/lib/styles.css";
+import ProductCard from "./ProductCard";
+
+const RelatedProducts: React.FC = () => {
+  const responsive = {
+    desktop: {
+      breakpoint: { max: 3000, min: 1024 },
+      items: 3,
+    },
+    tablet: {
+      breakpoint: { max: 1024, min: 464 },
+      items: 2,
+    },
+    mobile: {
+      breakpoint: { max: 767, min: 0 },
+      items: 1,
+    },
+  };
+  return (
+    <div className="mt-[50px] md:mt-[100px] mb-[100px] md:mb-0">
+      <div className="md:ml-[145px] text-[20px] font-bold mb-2">
+        You Might Also Like
+      </div>
+      <div>
+        <Carousel
+          responsive={responsive}
+          containerClass="mx-[140px]"
+          itemClass="px-[5px]"
+        >
+          <div>
+            <ProductCard
+              productName="Product Name"
+              price={50.0}
+              salePrice={40.0}
+              imageUrl="/product-1.webp"
+            />
+          </div>
+          <div>
+            <ProductCard
+              productName="Product Name"
+              price={50.0}
+              salePrice={40.0}
+              imageUrl="/product-1.webp"
+            />
+          </div>
+          <div>
+            <ProductCard
+              productName="Product Name"
+              price={50.0}
+              salePrice={40.0}
+              imageUrl="/product-1.webp"
+            />
+          </div>
+          <div>
+            <ProductCard
+              productName="Product Name"
+              price={50.0}
+              salePrice={40.0}
+              imageUrl="/product-1.webp"
+            />
+          </div>
+          <div>
+            <ProductCard
+              productName="Product Name"
+              price={50.0}
+              salePrice={40.0}
+              imageUrl="/product-1.webp"
+            />
+          </div>
+          <div>
+            <ProductCard
+              productName="Product Name"
+              price={50.0}
+              salePrice={40.0}
+              imageUrl="/product-1.webp"
+            />
+          </div>
+          <div>
+            <ProductCard
+              productName="Product Name"
+              price={50.0}
+              salePrice={40.0}
+              imageUrl="/product-1.webp"
+            />
+          </div>
+        </Carousel>
+      </div>
+    </div>
+  );
+};
+
+export default RelatedProducts;
--- src/app/product/[name]/page.tsx
+++ src/app/product/[name]/page.tsx
@@ -3,16 +3,17 @@ import ProductDetailsCarousel from "@/app/components/ProductDetailsCarousel";
 import Wrapper from "@/app/components/Wrapper";
 
 import { BiHeart } from "react-icons/bi";
+import RelatedProducts from "@/app/components/RelatedProducts";
 
 const ProductDetails: React.FC = () => {
   return (
     <div className="py-[100px] my-[100px] w-full md:pg-20">
       <Wrapper>
         <div className="ml-[70px] flex flex-col lg:flex-row md:px-10 gap-[50px] lg:gap:[100px]">
-          <div className="mx-[50px] w-full md:w-auto flex-[1.5] max-w-[500px] lg:max-w-full mx-auto lg:mx-0">
+          <div className="w-full md:w-auto flex-[1.5] max-w-[500px] lg:max-w-full mx-auto lg:mx-0">
             <ProductDetailsCarousel />
           </div>
-          <div className="flex-[1]">
+          <div className="mr-[0px] flex-[1]">
             <div className="text-[28px] font-semibold mb-2">
               Jordan Retro 6 G
             </div>
@@ -97,6 +98,7 @@ const ProductDetails: React.FC = () => {
             </div>
           </div>
         </div>
+        <RelatedProducts />
       </Wrapper>
     </div>
   );
