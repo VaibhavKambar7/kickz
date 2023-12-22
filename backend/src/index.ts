24725f3fa8e5a42fc5dcb6354109931cb82e5a65
VaibhavKambar7 <vaibhavkambar123@gmail.com>
2023-12-22 16:53:50 +0530

    created api to fetch all products from the db

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -3,8 +3,17 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
+import productRoute from "../routes/productRoute";
+
 const app: Express = express();
 const port = process.env.PORT || 3000;
+
+const cors = require('cors')
+app.use(cors())
+
+app.use(express.json())
+
+app.use("/api/products",productRoute)
 
 app.get("/", (req: Request, res: Response) => {
   res.send("hi there :)");
