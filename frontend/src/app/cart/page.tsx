39c87fe2e665e4bee5d4aa36e237bb43a56b22f5
VaibhavKambar7 <vaibhavkambar123@gmail.com>
2024-01-12 00:33:44 +0530

    implemented cart page ui

--- frontend/src/app/cart/page.tsx
+++ frontend/src/app/cart/page.tsx
@@ -6,26 +6,69 @@ import CartItem from "../components/CartItem";
 
 const Cart: React.FC = () => {
   return (
-    <div className="w-full md:py-20">
+    <div className="w-full md:py-[80px]">
       <Wrapper>
-        <div className="text-center max-w-[800px] mx-auto mt-8 md;mt-0">
+        <div className="text-center max-w-[800px] mx-auto mt-8 md:mt-0">
           <div className="text-[20px] md:text-[25px] mb-5 font-semibold leading-tight">
             Shopping Cart
           </div>
         </div>
 
-        <div className="flex flex-col lg:flex-row py-10 md:ml-[130px]">
+        <div className="flex flex-col lg:flex-row py-10 md:ml-[50px]">
           <div className="flex-[2]">
-            <div className="text-lg font-bold">Cart Items
-            <CartItem/>
-            <CartItem/>
-            <CartItem/>
+            <div className="text-lg font-bold mb-4">Cart Items</div>
+            <div className="space-y-4">
+              <CartItem />
+              <CartItem />
+              <CartItem />
             </div>
           </div>
 
-          <div className="flex-[1]">
-            <div className="text-lg font-bold">Summary</div>
+          <div className="flex-[1] ml-0 md:ml-16">
+            <div className="text-lg font-bold mb-4">Summary</div>
+
+            <div className="p-5 my-5 bg-black/[0.05] rounded-xl">
+              <div className="flex justify-between mb-4">
+                <div className="uppercase text-md md:text-lg font-medium text-black">
+                  Subtotal
+                </div>
+                <div className="text-md md:text-lg font-medium text-black">
+                  &#8377;XXX
+                </div>
+              </div>
+              <div className="text-sm md:text-md border-t pt-4">
+                The subtotal reflects the total price of your order, including
+                duties and taxes, before any applicable discounts. It does not
+                include delivery costs and international transaction fees.
+              </div>
+            </div>
+
+            <button className="w-full py-4 rounded-full bg-black text-white text-lg font-medium transition-transform active:scale-95 mb-3 hover:opacity-75 flex items-center gap-2 justify-center">
+              Checkout
+            </button>
           </div>
+        </div>
+
+        <div className="flex-[2] flex flex-col items-center pb-[50px] md:-mt-14">
+          <Image
+            src="/empty-cart.jpg"
+            alt="/empty-cart.jpg"
+            width={300}
+            height={300}
+            className="w-[300px] md:w-[400px] mb-4"
+          />
+          <span className="text-xl font-bold">Your cart is empty</span>
+          <span className="text-center mt-4">
+            Looks like you have not added anything in your cart.
+            <br />
+            Go ahead and explore top categories.
+          </span>
+          <Link
+            href="/"
+            className="py-4 px-8 rounded-full bg-black text-white text-lg font-medium transition-transform active:scale-95 mb-3 hover:opacity-75 mt-8"
+          >
+            Continue Shopping
+          </Link>
         </div>
       </Wrapper>
     </div>
--- frontend/src/app/components/CartItem.tsx
+++ frontend/src/app/components/CartItem.tsx
@@ -1,7 +1,85 @@
+import Image from "next/image";
 import React from "react";
+import { RiDeleteBin6Line } from "react-icons/ri";
 
-const CartItem: React.FC = () => {
-  return <div>CartItem</div>;
+const CartItem = ({}) => {
+  return (
+    <div className="flex py-5 gap-3 md:gap-5 border-b">
+      <div className="shrink-0 aspect-square w-[50px] md:w-[120px]">
+        <Image
+          src="https://res.cloudinary.com/dotxtkk4n/image/upload/v1702914336/kickz/p2/9cb30549-fe13-4998-ad7d-6c2876be3b5b_d0c6ql.webp"
+          alt="Product Image"
+          width={120}
+          height={120}
+        />
+      </div>
+
+      <div className="w-full flex flex-col">
+        <div className="flex flex-col md:flex-row justify-between">
+          <div className="text-lg md:text-2xl font-semibold text-black/[0.8]">
+            Jordan Retro 6 G
+          </div>
+
+          <div className="text-sm md:text-md font-medium text-black/[0.5] block md:hidden">
+            Mens Golf Shoes
+          </div>
+
+          <div className="text-sm md:text-md font-bold text-black/[0.5] mt-2">
+            MRP 19000
+          </div>
+        </div>
+
+        <div className="text-md font-medium text-black/[0.5] hidden md:block">
+          Mens Golf Shoes
+        </div>
+
+        <div className="flex items-center justify-between mt-4">
+          <div className="flex items-center gap-2 md:gap-10 text-black/[0.5] text-sm md:text-md">
+            <div className="flex items-center gap-1">
+              <div className="font-semibold">Size:</div>
+              <select
+                className="hover:text-black"
+              >
+                <option value="1">UK 6</option>
+                <option value="1">UK 6</option>
+                <option value="1">UK 6</option>
+                <option value="1">UK 6</option>
+                <option value="1">UK 6</option>
+                <option value="1">UK 6</option>
+                <option value="1">UK 6</option>
+                <option value="1">UK 6</option>
+              </select>
+            </div>
+
+            <div className="flex items-center gap-1">
+              <div className="font-semibold">Quantity:</div>
+              <select
+                className="hover:text-black"
+                // onChange={(e) => updateCartItem(e, "quantity")}
+              >
+                <option value="1">1</option>
+                <option value="1">1</option>
+                <option value="1">1</option>
+                <option value="1">1</option>
+                <option value="1">1</option>
+                <option value="1">1</option>
+                <option value="1">1</option>
+                <option value="1">1</option>
+                <option value="1">1</option>
+                <option value="1">1</option>
+              </select>
+            </div>
+          </div>
+          <RiDeleteBin6Line
+            // onClick={() =>
+            //     dispatch(removeFromCart({ id: data.id }))
+            // }
+            className="cursor-pointer text-black/[0.5] hover:text-black text-[16px] md:text-[20px]"
+          />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default CartItem;
